70f1c6251e363ca04d0d5f377f1930f1a3f50da6
Maximilian Rapp <maximilian.rapp@tum.de>
2026-05-07 19:30:32 +0200

    feat: Add HelloController with a simple hello endpoint

---
+++ core/src/main/java/de/devops26/kontor/core/HelloController.java
@@ -0,0 +1,13 @@
+package de.devops26.kontor.core;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+}

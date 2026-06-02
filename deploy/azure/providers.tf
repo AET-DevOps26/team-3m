975311d9d9cd8d49c675fe08c3d9efcf7f98314c
Maximilian Rapp <maximilian.rapp@tum.de>
2026-06-02 21:59:51 +0200

    feat(azure): add GitHub workflows for Azure VM deployment and teardown

--- deploy/azure/providers.tf
+++ deploy/azure/providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "azure-vm.tfstate"
+    # resource_group_name and storage_account_name are passed at init time
+  }
 }
 
 provider "azurerm" {
